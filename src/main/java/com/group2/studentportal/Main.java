2565b3a22209072e863d7ff12b896cb1805e338f
Marco Bautista <marcobautista2836@gmail.com>
2025-11-13 21:22:52 +0800

    initial commit

---
+++ src/main/java/com/group2/studentportal/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.group2.studentportal;
+
+/**
+ *
+ * @author Marco Bautista
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
